f79d752eddfa293b5a2860c96865bdb1b967740c
Flavins <maduabuchiokonkwo@gmail.com>
2017-10-26 00:48:26 +0100

    populate the api

--- app.js
+++ app.js
@@ -28,6 +28,18 @@ app.post('/product', function(request, response){
     })
 });
 
+app.get('/wishlist', function(request, response){
+    WishList.find({}).populate({path:'products', model: 'Product'}).exec(function(err, wishLists){
+        if(err){
+            response.status(500).send({error:"could not pfetch wishlist"});
+        }else{
+            response.status(200).send(wishLists);
+
+        }
+    })
+})
+    
+
 app.get('/product', function(request, response){
     products.find({}, function(err, products){
         if(err){
@@ -40,6 +52,33 @@ app.get('/product', function(request, response){
     })
 })
 
+app.post('/wishlist', function(request, responds){
+    var wishList = new WishList();
+    wishList.title = request.body.title;
+
+    wishList.save(function(err, newWishList){
+        if(err){
+            responds.status(500).send({error: "can not create a new wish list"})
+        }else{
+            responds.send(newWishList);
+        }
+    })
+})
+app.put('wishlist/product/add', function(request, response){
+    Product.findOne({id: request.body.productId}, function(err, product){
+        if(err){
+            response.status(500).send({error:"could not add item to the wishlist"});
+        }else{
+            WishList.update({_id:request.body.wishListId}, {$addToSet:{products: product._id}}, function(err, wishList){
+                if(err){
+                    response.status(500).send({error:"could not add item to wishlist"});
+                }else{
+                    response.send(wishList);
+                }
+            })
+        }
+    })
+})
 
 
 app.listen(3000, function(){
